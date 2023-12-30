2577caa89c161b30b6c9a1c746bde9ae6ad3bb06
XuHaijwill <zuiwoxuanlou@163.com>
2023-12-30 21:27:56 +0800

    add swagger

---
+++ admin/src/main/java/com/shop/admin/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.shop.admin.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @ClassName SwaggerConfig
+ * @Description api页面 /doc.html
+ * http://localhost:8001/doc.html#/documentManager/GlobalParameters-default
+ * @Author XuHaijwill
+ * @Date 2023/12/30 21:19
+ * @Version 1.0
+ **/
+@Configuration(proxyBeanMethods = false)
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    //基本信息的配置，信息会在api文档上显示
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Shop商城测试的接口文档")
+                .description("Shop商城相关接口的文档")
+                .termsOfServiceUrl("http://localhost:8001/hello")
+                .version("1.0")
+                .build();
+    }
+
+}
